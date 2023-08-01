e5008a83d616d37e8be973cbe9518068c2fc0ad4
darthvaderE <141022970+darthvaderE@users.noreply.github.com>
2023-08-01 22:09:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -54,9 +54,9 @@ def main():
     st.write("Source: Propst, E.J. and Wolter, N.E. (2023), Myer-Cotton Grade of Subglottic Stenosis Depends on Style of Endotracheal Tube Used. The Laryngoscope. https://doi.org/10.1002/lary.30577")
     st.write("Adapted by: Darth Vader a.k.a Eishaan Bhargava https://twitter.com/darthekb")
 
-image = Image.open('vader.jpg')
+    image = Image.open('vader.jpg')
 
-st.image(image, caption='Adapted by: Darth Vader a.k.a Eishaan Bhargava https://twitter.com/darthekb')
+    st.image(image, caption='Adapted by: Darth Vader a.k.a Eishaan Bhargava https://twitter.com/darthekb')
 
 if __name__ == '__main__':
     # Use the os module to dynamically set the port
