9371da21ce58fb150ef9b5e12eeed778002457e6
darthvaderE <141022970+darthvaderE@users.noreply.github.com>
2023-08-01 22:22:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def main():
     image_path = "vader.jpg"
     image = Image.open(image_path)
 
-    st.image(image, caption='Adapted by: Darth Vader a.k.a Eishaan Bhargava https://twitter.com/darthekb', width=1)
+    st.image(image, caption='Adapted by: Darth Vader a.k.a Eishaan Bhargava https://twitter.com/darthekb')
 
 if __name__ == '__main__':
     # Use the os module to dynamically set the port
